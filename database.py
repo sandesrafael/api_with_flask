795da899ae824daa56e69aa0a231f68e51ea603c
Rafael Sandes <raafaelsandes@hotmail.com>
2021-12-03 16:36:18 -0300

    __pycache__

---
+++ database.py
@@ -0,0 +1,2 @@
+def insert_user(name, email, password):
+    return {"id": 1, "name": name, "email": email}
---
+++ routes.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+from database import insert_user
+
+app = Flask("Youtube")
+
+
+@app.route("/helloworld", methods=["GET"])
+def hello_world():
+    return {"Hello": "World"}
+
+
+@app.route("/register/user", methods=["POST"])
+def register_user():
+    body = request.get_json()
+
+    if "name" not in body:
+        return generate_response(400, "The parameter 'name' is required.")
+
+    if "email" not in body:
+        return generate_response(400, "The parameter 'email' is required.")
+
+    if "password" not in body:
+        return generate_response(400, "The parameter 'password' is required.")
+
+    user_name = insert_user(body["name"], body["email"], body["password"])
+
+    return generate_response(200, "Created User", 'user', user_name)
+
+
+def generate_response(status, message, content_name=False, content=False):
+    response = {"status": status, "message": message}
+
+    if content_name and content:
+        response[content_name] = content
+
+    return response
+
+
+if __name__ == '__main__':
+    app.run()
